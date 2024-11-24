f3c0dfb67314c420166867232389c8a9ca791951
Pankajesc <pankajkumaryadavv87@gmail.com>
2024-11-24 10:16:42 +0530

    Initial commit

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -1,36 +1,34 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Slider from 'react-slick'; // Make sure you have react-slick installed
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './HeroSlider.css';
 
 const HeroSlider = () => {
-  useEffect(() => {
-    // Initialize slick on component mount
-    $(document).ready(function () {
-      $('.hero-slider').slick({
-        dots: true, // Enable dots
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 4000, // Adjust timing of the slides
-      });
-    });
-  }, []);
+  // Settings for the react-slick slider
+  const settings = {
+    dots: true,            // Enable dots
+    infinite: true,        // Infinite loop
+    speed: 500,            // Transition speed
+    slidesToShow: 1,       // Show 1 slide at a time
+    slidesToScroll: 1,     // Scroll 1 slide at a time
+    autoplay: true,        // Enable autoplay
+    autoplaySpeed: 4000,   // Adjust timing of the slides
+  };
 
   return (
     <div className="hero-slider">
-      <div className="slide">
-        <img src="image1.jpg" alt="Slide 1" />
-      </div>
-      <div className="slide">
-        <img src="image2.jpg" alt="Slide 2" />
-      </div>
-      <div className="slide">
-        <img src="image3.jpg" alt="Slide 3" />
-      </div>
+      <Slider {...settings}>
+        <div className="slide">
+          <img src="image1.jpg" alt="Slide 1" />
+        </div>
+        <div className="slide">
+          <img src="image2.jpg" alt="Slide 2" />
+        </div>
+        <div className="slide">
+          <img src="image3.jpg" alt="Slide 3" />
+        </div>
+      </Slider>
     </div>
   );
 };
